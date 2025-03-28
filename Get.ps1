578a6ab6510ed09970717266447b902a097cc069
Phil <info@philipp-sponsel.de>
2025-03-28 12:12:42 +0100

    init commit tested works

---
+++ Get.ps1
@@ -0,0 +1,103 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $MusicPath,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $SoundcloudClient_ID
+)
+
+Write-Host ("Loading Tracklist...") -ForegroundColor Blue
+try {
+    $Tracklist = Get-ChildItem -Path $MusicPath
+    $Tracklist | ForEach-Object {
+        $_ | Add-Member -NotePropertyName "TrackID" -NotePropertyValue (($_.Name).split("-")[-1]).split(".")[0]
+    }
+    Write-Host (" -> Done! " + $Tracklist.count + " ID's loaded") -ForegroundColor Green
+}
+catch {
+    Write-Host (" -> ERROR: " + $_.Exception.Message) -ForegroundColor Red
+    Exit 1
+}
+Write-Host ""
+
+
+Write-Host ("Getting Track Infos from Soundcloud...") -ForegroundColor Blue
+$Tracklist | ForEach-Object -Parallel {
+    $CurrentTrack = $_
+    try {
+        $Header = @{
+            accept = "application/json"
+        }
+        $TrackInfo = Invoke-RestMethod -Uri ("https://api-v2.soundcloud.com/tracks/" + $_.TrackID + "?client_id=" + $using:SoundcloudClient_ID) -Headers $Header
+        $_ | Add-Member -NotePropertyName "TrackInfo" -NotePropertyValue $TrackInfo -Force
+        if ($null -ne $_.Trackinfo.artwork_url -and $_.Trackinfo.artwork_url -ne "") {
+            Invoke-WebRequest -Uri ($_.Trackinfo.artwork_url.Replace("-large.jpg","-t500x500.jpg")) -OutFile ($using:PSScriptRoot + "/img/" + $_.TrackID + ".jpg")
+        }
+    }
+    catch {
+        Write-Host (" -> ERROR: Could not get track info for " + $CurrentTrack.Name + " : " + $_.Exception.Message) -ForegroundColor Red
+    }
+} -ThrottleLimit 20
+Write-Host (" -> Done! " + $Tracklist.TrackInfo.count + "/" + $Tracklist.count + " Trackinfos Loaded") -ForegroundColor Green
+Write-Host ""
+
+
+Write-Host ("Preparing to write Tags..") -ForegroundColor Blue
+try {
+    # Load the DLL
+    Add-Type -Path ($PSScriptRoot + "/TagLibSharp.dll") -ErrorAction Stop
+    Write-Host " -> TagLib# loaded successfully!" -ForegroundColor Green
+}
+catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+    exit 1
+}
+Write-Host ""
+
+
+Write-Host ("Adding ID3 Tags to Tracks...") -ForegroundColor Blue
+$Tracklist | ForEach-Object -Parallel {
+    if ($null -ne $_.Trackinfo -or $_.Trackinfo -ne "") {
+        $CurrentTrack = $_
+        try {
+            # Open the MP3 file using TagLib
+            $file = [TagLib.File]::Create($_.FullName)
+
+            # Write basic ID3 tags
+            $file.Tag.Title = $_.TrackInfo.title
+            #$file.Tag.Album = $_.TrackInfo.title
+            $file.Tag.Artists = $_.TrackInfo.user.username
+            $file.Tag.Year = $_.TrackInfo.display_date.year
+            $file.Tag.Genres = $_.TrackInfo.genre
+            #$file.Tag.Track = 1
+            #$file.Tag.Comment = "This is a comment."
+
+            # Load the image file and create a Picture object
+            $imageData = [System.IO.File]::ReadAllBytes(($using:PSScriptRoot + "/img/" + $_.TrackID + ".jpg"))
+            $picture = New-Object TagLib.Picture
+            $picture.Data = $imageData
+            $picture.MimeType = "image/jpeg"  # Change to "image/png" if using a PNG
+            $picture.Type = [TagLib.PictureType]::FrontCover  # Type of picture (e.g., Front Cover)
+
+            # Clear existing pictures (optional) and add the new one
+            $file.Tag.Pictures = @()
+            $file.Tag.Pictures += $picture
+
+            # Save the changes
+            $file.Save()
+
+            # Clean up
+            $file.Dispose()
+            Write-Host (" -> OK: '" + $CurrentTrack.Name + "'")-ForegroundColor Green
+        }
+        catch {
+            Write-Host (" -> Error for track '" + $CurrentTrack.Name + "': " + $_.Exception.Message )-ForegroundColor Red
+        }
+    }
+    else {
+        Write-Host (" -> Skipped " + $_.Name + "because not ID3 Tags available") -ForegroundColor Yellow
+    }
+} -ThrottleLimit 10
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Soundcloud-ID3-Scraper
+
+## How it Works.
+Assuming your Filenames look like this:
+```
+BuntesKaos - Der Letzte Tag Auf Erden-195578401.mp3
+Burn In Noise - A Message to Shankra Festival 2015-201219890.mp3
+Burn In Noise - Dance Temple 09 - Boom Festival 2014-181643154.mp3
+Bämmler's Proviant für'n Badestrand 2017-335101776.mp3
+CIRCUIT BREAKERS _ Nano Records Series Vol.8 _ 17_01_2015-187353477.mp3
+CLAPCAST #491-1988792703.mp3
+Charlz Beth - Inseltape #38-961481419.mp3
+Chet Faker - Thinking In Textures (Full Album)-125755178.mp3
+```
+the most im portant thign is the Track id with a `-` infront of it, e.g. `-195578401` and there must be somethinginfornt of it even if it is just `track-195578401`
+
+### Start Parameters
+| Parameter | Mandatory | Description |
+| --- | --- | --- |
+|-MusicPath| x | Path to music e.g. `/data/music` |
+|-$SoundcloudClient_ID| x | SoundCloud Client ID get it from browser session is easily findable |
